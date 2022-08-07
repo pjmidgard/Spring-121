5e9a8bd380e4ce457e68ad6243d1eea9ffea3419
Java Software <pj.midgard@yahoo.com>
2022-08-07 20:38:28 +0100

    Update Spring-121.py

--- Spring-121.py
+++ Spring-121.py
@@ -42,7 +42,7 @@ class compression:
                     	Deep_long=80
                     	Deep_long_All=Deep_long*1023
                     	block_size_long=1023
-                    	Times_compression=100
+                    	Times_compression=10
                     elif name[long-4:long]!=".doc":
                     	Deep_long=120
                     	Deep_long_All=Deep_long*31
@@ -464,7 +464,7 @@ class compression:
                     	Deep_long=80
                     	Deep_long_All=Deep_long*1023
                     	block_size_long=1023
-                    	Times_compression=100
+                    	Times_compression=10
                     elif nameas[long-4:long]!=".doc":
                     	Deep_long=120
                     	Deep_long_All=Deep_long*31
